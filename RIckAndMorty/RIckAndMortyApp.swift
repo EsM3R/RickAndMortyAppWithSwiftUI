14e92cf772238a011bbd624b414b50f719fb0e19
Mehmet Vural <105605335+EsM3R@users.noreply.github.com>
2024-05-19 21:52:41 +0300

    Initial Commit

---
+++ RIckAndMorty/RIckAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIckAndMortyApp.swift
+//  RIckAndMorty
+//
+//  Created by Mehmet Vural on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RIckAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
